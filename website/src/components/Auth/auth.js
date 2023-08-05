5df12fdf529b271b9a733dd21b6ab9b1df04613b
ndgsghdj <75611248+ndgsghdj@users.noreply.github.com>
2023-08-05 17:36:53 +0800

    partial auth

---
+++ website/src/components/Auth/auth.js
@@ -0,0 +1,35 @@
+import { React, useState } from "react";
+import { auth } from "../../config/firebase";
+import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err)
+    }
+  };
+
+  const logOut = async () => {
+    try {
+        await signOut(auth);
+    } catch (err) {
+        console.error(err)
+    }
+  }
+
+  return (
+    <div>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={signIn}>Sign In</button>
+      <button onClick={logOut}></button>
+    </div>
+  );
+};
+
+export default Auth;
